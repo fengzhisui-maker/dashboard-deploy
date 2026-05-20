2b5fc8d70dd4ec177f5b9abd6c0dc08b9ad931d6
fengzhisui-maker <fengzhisui@gmail.com>
2026-05-21 02:22:38 +0800

    progress: 130/320 videos

--- pending_queue.js
+++ pending_queue.js
@@ -10,7 +10,7 @@ var PENDING_QUEUE = [
     "limitDays": 365,
     "autoTrack": true,
     "status": "processing",
-    "videosDone": 120,
+    "videosDone": 130,
     "createdAt": "2026-05-20T16:44:27.695Z"
   }
 ];
