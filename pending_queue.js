74e6249db28ea978f28ff9e79ee55d61d5e6aca7
fengzhisui-maker <fengzhisui@gmail.com>
2026-05-21 00:44:28 +0800

    queue: add bilibili UP主 期神附体-期货

--- pending_queue.js
+++ pending_queue.js
@@ -1,1 +1,15 @@
-var PENDING_QUEUE = [];
+var PENDING_QUEUE = [
+  {
+    "id": 1779295467695,
+    "action": "add_up",
+    "platform": "bilibili",
+    "name": "期神附体-期货",
+    "upId": "1473394951",
+    "url": "https://space.bilibili.com/1473394951",
+    "limitCount": 500,
+    "limitDays": 365,
+    "autoTrack": true,
+    "status": "queued",
+    "createdAt": "2026-05-20T16:44:27.695Z"
+  }
+];
